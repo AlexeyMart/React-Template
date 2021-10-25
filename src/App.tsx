28d2d0d72b7bef1521a22c43ff4332393880d8ab
Alexey Martyshev <a.martyshev@rambler-co.ru>
2021-10-25 16:01:56 +0300

    css-loader, images, svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { FC } from "react";
 
+// Styles
+import "./styles.css";
+
+// Images
+import ReactImage from "./assets/images/favicon.png";
+
+// Svg
+import ReactLogo from "./assets/svg/react-logo.svg";
+
 const App: FC = () => {
-  return <h1>React Typescript Webpack Template</h1>;
+  return (
+    <div className="main">
+      <h1 className="main__title">React Typescript Webpack Template</h1>
+
+      <img src={ReactImage} alt="main__logo" className="image" />
+
+      <img src={ReactLogo} alt="main__logo" className="image" />
+    </div>
+  );
 };
 
 export default App;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
     ],
   },
   output: {
@@ -27,7 +39,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
-      favicon: "./src/favicon.png",
+      favicon: "./src/assets/images/favicon.png",
     }),
   ],
 };
